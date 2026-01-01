28931db408641fd5c7d15e9e07a17be119f4e9dc
Connor Shea <connor.james.shea@gmail.com>
2025-12-31 17:58:53 -0700

    Set up reproduction case.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+#!/usr/bin/env node
 
-export function App() {
-  return <div>Hello, world!</div>;
-}
+// If I try to apply the "disable rule for whole file" fix here,
+// it will go to the top of the file. I want to keep the
+// `#!/usr/bin/env node` line as the first thing in the file
+// so the file is valid.
+console.log("this isn't allowed :)");
